62a4c4ebe4dbaacfbec8d59806eb0319b2a607c3
PerlitaSolve <124049201@upq.edu.mx>
2025-11-03 18:50:36 -0600

    Crear Editar

---
+++ EditarTrans.js
@@ -0,0 +1,13 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { usState } from 'react'
+
+export default function EditarTrans() {
+    return (
+      <View>
+        <Text>EditarTrans</Text>
+      </View>
+    )
+  
+}
+
+const styles = StyleSheet.create({})
