e10f8620ed5967eff7e9d5ae6dfd5aae8e577cdd
124050316-ship-it <124050316@upq.edu.mx>
2025-11-03 18:59:47 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 
 import Autenticacion from './screens/Autenticacion'; // importar nuestra pantalla de contador 
-
+import Gastos from './screens/Gastos';
 // 2. Main: Zona de componentes
 export default function App() {
 
